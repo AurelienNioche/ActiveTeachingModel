85f86873a13e4111ae12252a9330b339e4bcb581
c <ctorre148@gmail.com>
2019-06-20 17:40:39 +0300

    ann update

--- learner/carlos_ann.py
+++ learner/carlos_ann.py
@@ -1,42 +1,37 @@
 import numpy as np
 
+np.random.seed(123)
+
 
 def main():
-    pass
+    """
+    Example one layer neural network with 4 fully connected and recurrent
+    units and two output neurons
+    """
+    neuron0 = Neuron(neuron_id=0, input_neurons=[0])
+    neuron1 = Neuron(neuron_id=1, input_neurons=[1])
+    neuron2 = Neuron(neuron_id=2, input_neurons=[2])
+    neuron3 = Neuron(neuron_id=3, input_neurons=[3])
+    neuron4 = Neuron(neuron_id=4, input_neurons=[0, 1, 2, 3, 4])
+    neuron5 = Neuron(neuron_id=5, input_neurons=[0, 1, 2, 3, 5])
 
 
 class Network:
     def __init__(self):
         self.connectivity_matrix = None
-        pass
+        self.n_neurons = 0
 
-    def connect(self):
-        raise NotImplementedError
+    # def connect(self, neuron_position):
+    #     if neuron_position * self.neuron_layout == 0:
+    #         raise LookupError("ERROR: neuron in layer " + neuron_position[0]
+    #               + " and position " + neuron_position[1]
+    #               + " in layer does not exist and cannot be connected.")
 
     def predict(self):
-        pass
+        raise NotImplementedError
 
     def train(self):
-        pass
-
-
-class FullyConnectedNetwork(Network):
-    """
-    Example one layer neural network with 4 fully connected and recurrent
-    units and two output neurons
-    """
-    def __init__(self):
-        self.neuron_layout = np.array([1, 1, 1, 1],
-                                      [1, 1, 0, 0])
-        # Matrix defined this way to be graphical #
-        super().__init__()
-
-    def connect(self, neuron_position):
-        if neuron_position * self.neuron_layout == 0:
-            print("ERROR: neuron in layer " + neuron_position[0]
-                  + " and position " + neuron_position[1]
-                  + " in layer does not exist and cannot be connected.")
-
+        raise NotImplementedError
 
 
 class Neuron:
@@ -56,17 +51,41 @@ class Neuron:
     the g function...
     """
 
-    def __init__(self, neuron_id, neuron_position, connectivity_matrix, tau, current=0):
+    def __init__(self, neuron_id, tau, network="network", input_neurons=None,
+                 current=0):
         self.neuron_id = neuron_id
-        self.neuron_position = neuron_position
-        self.connectivity_matrix = connectivity_matrix
+        self.inputs = None
+        self.weights = None
+
+        self.network = network
+        network.n_neurons += 1
+
+        self.input_neurons = input_neurons
+
+        # roles = ["input", "hidden", "output"]
+        # self.role = None
+        # if self.input_neurons is None:
+        #     self.role = roles[0]
+        # elif self.output_neurons is None:
+        #     self.role = roles[2]
+        # elif self.input_neurons is not None and self.output_neurons is not None:
+        #     self.role = roles[1]
+        # else:
+        #     raise AssertionError("Neuron does not have a valid role")
+
         self.current = current
         self.tau = tau
+        self.input_neurons = input_neurons
+
+    def initialize_attributes(self):
+        self.inputs = np.zeros(self.input_neurons.size)
+        self.weights = np.random.rand(1, self.input_neurons.size)
 
     def activation(self):
-        self.current = self.current * (-self.current + connectivity_matrix)
+        self.current = self.current * (-self.current + self.inputs * self.weights)
 
 
 if __name__ == "__main__":
-    neuron0 = Neuron(neuron_id=0, neuron_position=[0, 0])
     main()
+
+# TODO after long weekend please continue working in the activation function
