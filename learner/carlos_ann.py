55085759b640cc8104bac01a95c0bacd7b924184
c <ctorre148@gmail.com>
2019-06-27 16:28:36 +0300

    ann update

--- learner/carlos_ann.py
+++ learner/carlos_ann.py
@@ -12,20 +12,14 @@ def main():
     neuron1 = Neuron(neuron_id=1, input_neurons=[1])
     neuron2 = Neuron(neuron_id=2, input_neurons=[2])
     neuron3 = Neuron(neuron_id=3, input_neurons=[3])
-    neuron4 = Neuron(neuron_id=4, input_neurons=[0, 1, 2, 3, 4])
-    neuron5 = Neuron(neuron_id=5, input_neurons=[0, 1, 2, 3, 5])
+    neuron4 = Neuron(neuron_id=4, input_neurons=[0, 1, 2, 3, 4], role="output")
+    neuron5 = Neuron(neuron_id=5, input_neurons=[0, 1, 2, 3, 5], role="output")
 
 
 class Network:
     def __init__(self):
         self.connectivity_matrix = None
         self.n_neurons = 0
-
-    # def connect(self, neuron_position):
-    #     if neuron_position * self.neuron_layout == 0:
-    #         raise LookupError("ERROR: neuron in layer " + neuron_position[0]
-    #               + " and position " + neuron_position[1]
-    #               + " in layer does not exist and cannot be connected.")
 
     def predict(self):
         raise NotImplementedError
@@ -40,7 +34,8 @@ class Neuron:
     τc˙i(t)=−ci(t)+∑j=1NJij ⋅ rj(t)+ξi(t)    (1)
     ri=g(ci)     (2)
 
-    [[t * c(t+1) = -c_{i}(t) + \sum_{j = 1}^{N}{J_{i, j}} * r_{j}{t} + E_{i}(t)]]
+    [[t * c(t+1) = -c_{i}(t) + \sum_{j = 1}^{N}{J_{i, j}} * r_{j}{t}
+    + E_{i}(t)]]
     where c, r are respectively the synaptic currents and the firing rates,
     J the connectivity matrix, each ξi is an independent random variable having
     a gaussian distribution with mean zero and variance ξ0 and τ is a constant
@@ -51,41 +46,54 @@ class Neuron:
     the g function...
     """
 
-    def __init__(self, neuron_id, tau, network="network", input_neurons=None,
-                 current=0):
+    def __init__(self, neuron_id, tau, theta, gamma, network="network", input_neurons=None,
+                 role="hidden", current=0):
         self.neuron_id = neuron_id
-        self.inputs = None
+        self.input_currents = None
         self.weights = None
+        self.gain = np.empty(0)
 
         self.network = network
         network.n_neurons += 1
 
         self.input_neurons = input_neurons
 
-        # roles = ["input", "hidden", "output"]
-        # self.role = None
-        # if self.input_neurons is None:
-        #     self.role = roles[0]
-        # elif self.output_neurons is None:
-        #     self.role = roles[2]
-        # elif self.input_neurons is not None and self.output_neurons is not None:
-        #     self.role = roles[1]
-        # else:
-        #     raise AssertionError("Neuron does not have a valid role")
+        # Neuron role
+        roles = ["input", "hidden", "output"]
+        self.role = role
+        if self.role not in roles:
+            raise AssertionError("Neuron does not have a valid role")
+        if self.input_neurons is None:
+            self.role = roles[0]
+            print("Neuron instanced as role='input'")
 
         self.current = current
         self.tau = tau
         self.input_neurons = input_neurons
 
+        # Gain function
+        self.theta = theta
+        self.gamma = gamma  # always < 1; gain function is sublinear
+
     def initialize_attributes(self):
-        self.inputs = np.zeros(self.input_neurons.size)
+        self.input_currents = np.zeros(self.input_neurons.size)
         self.weights = np.random.rand(1, self.input_neurons.size)
 
+    def compute_gain(self):
+        if self.role == "input":
+            raise AssertionError("Input neurons lack the gain function")
+        for i in self.input_currents:
+            if i + self.theta > 0:
+                new_gain = (i + self.theta) ** self.gamma
+                self.gain = np.concatenate(self.gain, new_gain)
+            else:
+                self.gain = np.concatenate(self.gain, 0)
+
     def activation(self):
-        self.current = self.current * (-self.current + self.inputs * self.weights)
+        self.current =\
+            self.current * (-self.current + self.input_currents * self.weights
+                            * self.gain + np.random.rand() / self.tau)
 
 
 if __name__ == "__main__":
     main()
-
-# TODO after long weekend please continue working in the activation function
