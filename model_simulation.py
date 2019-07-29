d5395d3c101cfeb04342911908880fbe55af9bc6
AurelienNioche <nioche.aurelien@gmail.com>
2019-07-29 16:09:13 +0300

    update

--- model_simulation.py
+++ model_simulation.py
@@ -101,7 +101,8 @@ def main(t_max=300, n_item=30, teacher_model=None, verbose=False,
 
 if __name__ == "__main__":
 
-    for tm in (TraditionalLeitnerTeacher, AvyaTeacher, LeitnerTeacher,
+    for tm in (TraditionalLeitnerTeacher,
+               AvyaTeacher, LeitnerTeacher,
                RandomTeacher):
         main(teacher_model=tm, t_max=300, n_item=30,
              normalize_similarity=True)
