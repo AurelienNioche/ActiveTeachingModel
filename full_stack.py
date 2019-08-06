2fd8ac80f4a2a6f86b3b0fb10ce3c8ccd0707035
AurelienNioche <nioche.aurelien@gmail.com>
2019-08-06 10:48:59 +0300

    update

--- full_stack.py
+++ full_stack.py
@@ -1,7 +1,6 @@
-import numpy as np
 from tqdm import tqdm
 
-import multiprocessing as mp
+import os
 
 import plot.memory_trace
 import plot.success
@@ -19,12 +18,10 @@ from simulation.data import Data
 # from fit.bayesian import BayesianFit
 # from fit.bayesian_gpyopt import BayesianGPyOptFit
 # from fit.bayesian_pygpgo import BayesianPYGPGOFit
-from fit.fit import Fit
+# from fit.fit import Fit
 from fit.bayesian_pygpgo import BayesianPYGPGOFit
 
-import warnings
-
-from utils.utils import dic2string
+from utils.utils import dic2string, dump, load
 
 
 def run(student_model, teacher_model, student_param,
@@ -37,7 +34,7 @@ def run(student_model, teacher_model, student_param,
 
     learner = student_model(param=student_param, tk=teacher.tk)
 
-    iterator = tqdm(range(t_max))  # if verbose else range(t_max)
+    iterator = tqdm(range(t_max))
 
     model_learner = student_model(
         tk=teacher.tk,
@@ -63,25 +60,16 @@ def run(student_model, teacher_model, student_param,
         teacher.register_question_and_reply(question=question, reply=reply,
                                             possible_replies=possible_replies)
 
-        # print('Q&R',question, reply)
-
-        # Update the model of the learner
-
         data_view = Data(n_items=n_item,
                          questions=teacher.questions[:t+1],
                          replies=teacher.replies[:t+1],
                          possible_replies=teacher.possible_replies[:t+1, :])
 
         f.evaluate(max_iter=max_iter, data=data_view)
-        # if fit_r is not None:
+
         model_learner.set_parameters(f.best_param)
-        # else:
-        #     warnings.warn("Fit did not end up successfully :/")
 
         model_learner.learn(question=question)
-        # We assume that the matching is (0,0), (1, 1), (n, n)
-        # print(model_learner.d)
-        # print("IIII")
 
     p_recall = p_recall_over_time_after_learning(
         agent=learner,
@@ -97,27 +85,7 @@ def run(student_model, teacher_model, student_param,
     }
 
 
-def main(student_model=None, teacher_model=None,
-         student_param=None,
-         n_item=60, grades=(1, ), t_max=2000,
-         max_iter=10,
-         normalize_similarity=True):
-
-    if student_model is None:
-        student_model = ActRMeaning
-
-    if student_param is None:
-        student_param = {"d": 0.5, "tau": 0.01, "s": 0.06, "m": 0.02}
-
-    if teacher_model is None:
-        teacher_model = AvyaTeacher
-
-    r = run(
-        student_model=student_model, teacher_model=teacher_model,
-        student_param=student_param,
-        n_item=n_item, grades=grades, t_max=t_max,
-        normalize_similarity=normalize_similarity,
-        max_iter=max_iter)
+def _plot(r):
 
     seen = r['seen']
     p_recall = r['p_recall']
@@ -178,13 +146,48 @@ def main(student_model=None, teacher_model=None,
         line_width=line_width * 2
     )
 
+    save_fig(f"fullstack_{extension}.pdf")
+
+
+def main(student_model=None, teacher_model=None,
+         student_param=None,
+         n_item=60, grades=(1, ), t_max=2000,
+         max_iter=10,
+         normalize_similarity=True, force=False, plot_fig=True):
+
+    if student_model is None:
+        student_model = ActRMeaning
+
+    if student_param is None:
+        student_param = {"d": 0.5, "tau": 0.01, "s": 0.06, "m": 0.02}
+
+    if teacher_model is None:
+        teacher_model = AvyaTeacher
+
     extension = f'{teacher_model.__name__}_{student_model.__name__}_' \
         f'{dic2string(student_param)}_' \
         f'ni_{n_item}_grade_{grades}_tmax_{t_max}_' \
         f'norm_{normalize_similarity}_' \
         f'max_iter_{max_iter}'
 
-    save_fig(f"fullstack_{extension}.pdf")
+    bkp_file = os.path.join('bkp', 'full_stack', f'{extension}.p')
+
+    r = load(bkp_file)
+    if r is None or force:
+        r = run(
+            student_model=student_model,
+            teacher_model=teacher_model,
+            student_param=student_param,
+            n_item=n_item,
+            grades=grades,
+            t_max=t_max,
+            normalize_similarity=normalize_similarity,
+            max_iter=max_iter)
+
+        dump(r, bkp_file)
+
+    if plot_fig:
+        _plot(r)
 
 
 if __name__ == '__main__':
