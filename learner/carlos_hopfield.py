1dfdcada025f6c482b019c79490c7d14ccfe794b
ctorr148 <ctorre148@gmail.com>
2019-08-20 17:07:22 +0300

    update

--- learner/carlos_hopfield.py
+++ learner/carlos_hopfield.py
@@ -13,32 +13,26 @@ class Network:
     its different characteristics that can be recalled.
     """
     def __init__(self, n_neurons=100000, p=16, f=0.1, inverted_fraction=0.3,
-                 first_p=1):
+                 noise_variance=65, first_p=1):
         self.n_neurons = n_neurons
         self.p = p
         self.f = f
         self.first_p = first_p
         self.inverted_fraction = inverted_fraction
+        self.noise_variance = noise_variance
 
         self.weights = np.zeros((self.n_neurons, self.n_neurons))
 
         self.active_fraction = f
 
         self.initial_currents = np.zeros(self.n_neurons)
-            # np.random.choice([0, 1],
-            #                  p=[1 - self.active_fraction,
-            #                     self.active_fraction],
-            #                  size=self.n_neurons)
 
         self.patterns = \
             np.random.choice([0, 1], p=[1 - self.f, self.f],
                              size=(self.p, self.n_neurons))
         print("\nPatterns:\n", self.patterns)
 
-        self.currents = np.zeros(self.n_neurons)
-        # TODO get rid of self.last_currents
-        self.last_currents = np.zeros(self.n_neurons)
-        self.currents_history = np.zeros(self.n_neurons)
+        self.currents = np.zeros((1, self.n_neurons), dtype=int)
         self.patterns_evolution = None
 
     # def present_pattern(self, item):
@@ -69,22 +63,15 @@ class Network:
 
     def _initialize_currents(self):
 
-        self.initial_currents = self.distort_pattern(self.patterns[0],
-                                                     self.inverted_fraction)
+        self.currents = np.copy(self.distort_pattern(self.patterns[0],
+                                                       self.inverted_fraction))
+        #[1, 1, 1, 1, 1,0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-        print("\nInitial currents:\n", self.initial_currents)
-
-        self.currents = np.copy(self.initial_currents)
-        self.last_currents = np.copy(self.initial_currents)
-        self.currents_history = np.copy(self.initial_currents)
+        print("\nInitial currents:\n", self.currents)
 
     def _compute_weights(self):
         """
-        Weight values are calculated without any training.
-
-        For several patterns, calculate the matrix for the first pattern,
-        then calculate the value for the second matrix and finally add the
-        two matrices together.
+        Weight values are calculated by adding the matrices of each pattern.
         """
 
         print(f"...Computing weights...")
@@ -141,9 +128,13 @@ class Network:
 
         :param neuron: int neuron number
         """
-        dot_product = np.dot(self.weights[neuron], self.last_currents)
+        dot_product = np.dot(self.weights[neuron], self.currents[-2])
 
-        self.currents[neuron] = self._activation_function(dot_product)
+        # Amplitude-modulated Gaussian noise
+        noise = np.random.normal(loc=0, scale=self.noise_variance**0.5) * 0.075
+
+        self.currents[-1, neuron] = self._activation_function(dot_product
+                                                              + noise)
 
     def _update_all_neurons(self):
         """
@@ -162,24 +153,28 @@ class Network:
         updating goes on until the system is in a stable state. Note that the
         full network should be updated before the same node gets updated again.
         """
+        # self.last_currents = self.currents
 
         values = np.arange(0, self.n_neurons, 1)
         update_order = np.random.choice(values, self.n_neurons, replace=False)
 
+        self.currents = np.vstack((self.currents, np.zeros(self.n_neurons)))
+
         for i in update_order:
             self._update_current(i)
 
-        self.currents_history = np.vstack((self.currents_history,
-                                           self.currents))
+        # self.currents_history = np.vstack((self.currents_history,
+        #                                    self.currents))
 
-    def _compute_patterns_evolution(self):
-
-        for p in range(self.p):
-            similarity = np.sum(self.currents == self.patterns[p])
-            if not self.patterns_evolution:
-                self.patterns_evolution = similarity
-            else:
-                np.vstack((self.patterns_evolution, similarity))
+    # def _compute_patterns_evolution(self):
+    #
+    #     for p in range(self.p):
+    #         similarity = np.sum(self.currents[-1] == self.patterns[p])
+    #         if not self.patterns_evolution:
+    #             self.patterns_evolution = similarity
+    #         else:
+    #             self.patterns_evolution = \
+    #                 np.vstack((self.patterns_evolution, similarity))
 
     def _find_attractor(self):
         """
@@ -187,18 +182,17 @@ class Network:
         stop updating. If a complete network update does not change any of the
         node values, then you are at an attractor so you can stop.
         """
-        # TODO review, always 2 iterations
-        i = 1
+        tot = 1
 
-        assert np.sum(self.currents - self.last_currents) != 0
-        while (self.currents != self.last_currents).all():  # np.sum(self.currents - self.last_currents) != 0:
-            self.last_currents = self.currents
+        while (self.currents[-1] != self.currents[-2]).all() or tot < 2:  # np.sum(self.currents - self.last_currents) != 0:
             self._update_all_neurons()
-            self._compute_patterns_evolution()
-            i += 1
-            print(f"Update {i} finished.")
+            # self._compute_patterns_evolution()
+            tot += 1
+            print(f"Update {tot} finished.")
 
-        print(f"\nFinished as attractor {self.currents} after {i} "
+        attractor = np.int_(np.copy(self.currents[-1]))
+
+        print(f"\nFinished as attractor {attractor} after {tot} "
               f"node value updates.")
 
     def simulate(self):
@@ -211,10 +205,31 @@ class Network:
         self._update_all_neurons()
         self._find_attractor()
 
+    def p_recall(self, item, time=None):
+
+        # bin_question is the partial (distorted) array
+        question_array = np.array([item])
+        bin_question = ((question_array[:, None]
+                         & (1 << np.arange(8))) > 0).astype(int)
+        bin_question = np.append(bin_question, np.zeros(self.n_neurons
+                                                        - bin_question.size))
+
+        print("Item given as pattern:", bin_question)
+
+        self.initial_currents = bin_question
+        self._update_all_neurons()
+
+        match = np.sum(self.last_currents == self.initial_currents)
+        p_r = match / self.n_neurons
+        print("Current after item presentation and one update:", self.currents)
+        print("Probability of recall of the item: ", p_r)
+
+        return p_r
+
 
 def plot(network):
 
-    data = network.currents_history
+    data = network.currents
 
     fig, ax = plt.subplots()
     im = ax.imshow(data)
@@ -223,11 +238,25 @@ def plot(network):
     ax.yaxis.set_major_locator(MaxNLocator(integer=True))
 
     plt.setp(ax.get_xticklabels(), rotation=90, ha="right",
-             rotation_mode="anchor")  # , fontsize=font_size)
+             rotation_mode="anchor")
 
     ax.set_title("Network currents history")
     ax.set_xlabel("Neuron")
     ax.set_ylabel("Iteration")
+
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_weights(network):
+
+    fig, ax = plt.subplots()
+    plt.contourf(network.weights)
+    ax.set_aspect("auto")
+
+    ax.set_title("Weights matrix")
+    ax.set_xlabel("Neuron id")
+    ax.set_ylabel("Neuron id")
 
     plt.tight_layout()
     plt.show()
@@ -261,7 +290,7 @@ def main(force=False):
 
     if not os.path.exists(bkp_file) or force:
 
-        np.random.seed(1234)
+        np.random.seed(12345)
 
         # flower = {"kanji": np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
         #           "meaning": np.array([1, 1, 1, 0, 0, 0, 0, 0, 0, 1])}
@@ -273,10 +302,10 @@ def main(force=False):
         #        "meaning": np.array([0, 0, 0, 0, 1, 0, 1, 1, 1, 1])}
 
         network = Network(
-                            n_neurons=14,
+                            n_neurons=10,
                             f=0.4,
-                            p=2,
-                            inverted_fraction=0.9
+                            p=4,
+                            inverted_fraction=0.3
                          )
 
         # network.present_pattern(flower)
@@ -284,12 +313,14 @@ def main(force=False):
         # network.present_pattern(eye)
 
         network.simulate()
+        # network.p_recall(22)
         pickle.dump(network, open(bkp_file, "wb"))
     else:
         print("Loading from pickle file...")
         network = pickle.load(open(bkp_file, "rb"))
 
     plot(network)
+    plot_weights(network)
     # plot_average_firing_rate(network)
 
 
