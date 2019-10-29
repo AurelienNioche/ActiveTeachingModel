8807d350a3ce0281fea26a349266d0ce2a57efe0
Aurélien Nioche <nioche.aurelien@gmail.com>
2019-10-29 22:46:22 +0200

    update

--- memory.py
+++ memory.py
@@ -9,64 +9,70 @@ def f(t, t_r, b, a=1):
     return y
 
 
+def proof_of_concept():
+
+    success_rate = 0.9
+
+    base_delta = 2
+
+    b = - np.log(success_rate)
+    alpha = 1 + np.log(success_rate) / (base_delta*b)
+
+    print(f"b = {b}, alpha={alpha}")
+
+    delta = np.array([(base_delta**n) for n in range(0, 10)])
+
+    for i, d in enumerate(delta):
+        print(f"delta={d}; p: {np.exp(-b * d)}")
+        b *= (1 - alpha)
+
+
 def main():
 
+    base_delta = 2
     success_rate = 0.9
-
-    b = - np.log(success_rate)
-    alpha = 1 + np.log(success_rate) / (2*b)
-    print(b, alpha)
-
-    pres = np.array([0, 1, ] + [(2**n) for n in range(1, 10)])
-    print("pres", pres)
-
-    for i, delta in enumerate(pres):
-        if i == 0:
-            pass
-        else:
-            b *= (1 - alpha)
-        print(np.exp(-b*delta))
-
-    success_rate = 0.9
-
-    b = - np.log(success_rate)
-    alpha = 1 + np.log(success_rate) / (2*b)
-
-    flag = False
-
-    y=[]
     n_iteration = 100
 
+    delta = np.array([(base_delta ** n) for n in range(0, 10)])
+    print("delta", delta)
+
+    b = - np.log(success_rate)
+    alpha = 1 + np.log(success_rate) / (base_delta*b)
+    print(f"b = {b}, alpha={alpha}\n")
+
+    y = []
+
     current_idx = 0
-    delta = 0
+    d = 0
+    pres = []
     for t in range(0, n_iteration):
 
-        if delta == pres[current_idx]:
-            print("delta == pres", t)
-            print("delta", delta)
-            flag = True
-
-        p = np.exp(-b*delta)
-        print(f"t={t}; delta={delta}; p={p:.3f}")
+        p = np.exp(-b*d)
+        print(f"t={t}; delta={d}; p={p:.3f}")
 
         y.append(p)
-        delta += 1
 
-        if flag is True:
-            if t == 0:
-                pass
-            else:
-                b *= (1 - alpha)
-                delta = 0
-
+        if d == delta[current_idx]:
+            b *= (1 - alpha)
+            d = 0
             current_idx += 1
-            flag = False
+            pres.append(t)
+            print("***")
+
+        else:
+            d += 1
 
     fig, ax = plt.subplots()
+    for pr in pres:
+        ax.axvline(pr+0.5, linestyle="--", color="0.2",
+                   linewidth=0.5)
     ax.plot(y)
-    ax.axhline(0.9, linestyle="--", color="0.2")
+    # ax.axhline(0.9, linestyle="--", color="0.2")
     ax.set_ylim(0, 1)
     ax.set_xlim(0, n_iteration)
+
+    plt.tight_layout()
+
     plt.show()
 
 
