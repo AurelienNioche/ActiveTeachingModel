61906a654dfb40e450b056aec3ca70aa1f56550a
Aurélien Nioche <nioche.aurelien@gmail.com>
2019-10-29 20:12:10 +0200

    update

--- memory.py
+++ memory.py
@@ -2,43 +2,68 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def f(t, t_r, b):
-
-    y = np.exp(-b*(t-t_r))
+def f(t, t_r, b, a=1):
+    print("delta", t-t_r)
+    y = a * np.exp(-b*(t-t_r))
 
     return y
 
 
 def main():
-    b = 0.02
-    t_r = 0
+
+    success_rate = 0.9
+
+    b = - np.log(success_rate)
+    alpha = 1 + np.log(success_rate) / (2*b)
+    print(b, alpha)
+
+    pres = np.array([0, ] + [(2**n) for n in range(1, 10)])
+    print("pres", pres)
+
+    for i, delta in enumerate(pres):
+        if i == 0:
+            pass
+        else:
+            b *= (1 - alpha)
+        print(np.exp(-b*delta))
+
+    success_rate = 0.9
+
+    b = - np.log(success_rate)
+    alpha = 1 + np.log(success_rate) / (2*b)
+
+    flag = False
+
     y=[]
-    alpha=0.8
-    n_iteration = 10000
+    n_iteration = 100
 
-    presentation = []
-    l = 0.1
-    for t in range(10):
-        presentation.append(int(5**(10*l)))
-        l += 0.10
-    print(presentation)
-        # np.random.choice(n_iteration, 5, replace=False)
+    current_idx = 0
+    delta = 0
+    for t in range(0, n_iteration):
 
-    for t in range(n_iteration):
+        if delta == pres[current_idx]:
+            print("delta == pres", t)
+            print("delta", delta)
 
-        if t in presentation:
-        # if t%200 == 0:
-            t_r = t
-            b *= (1-alpha)
+            b *= (1 - alpha)
+            flag = True
 
-        y.append(f(t=t, t_r=t_r, b=b))
+        p = np.exp(-b*delta)
+        print(f"t={t}; p={p:.3f}")
+
+        y.append(p)
+        delta += 1
+
+        if flag is True:
+
+            delta = 0
+            current_idx += 1
+            flag = False
 
     fig, ax = plt.subplots()
-    ax.plot(y, linewidth=2)
-    for t in presentation:
-        ax.axvline(t, linestyle="--", color='0.1', zorder=-10)
+    ax.plot(y)
+    ax.axhline(0.9, linestyle="--", color="0.2")
     ax.set_ylim(0, 1)
-    ax.set_xlim(0, n_iteration)
     plt.show()
 
 if __name__=="__main__":
