f7ef9614b75d210409a49321b905f4d6d5c8476f
c-torre <ctorre@mailbox.org>
2020-08-06 19:48:42 +0300

    fake data good shape

--- plot/multi_scatter.py
+++ plot/multi_scatter.py
@@ -13,29 +13,26 @@ from numpy.random import default_rng
 
 import paths
 
-FIG_DIR = paths.FIG_DIR
 
 rng = default_rng()
 
+MODELS = frozenset({"learner", "psychologist", "teacher"})
+
+LEARNERS = frozenset({"exponential", "walsh2018"})
+PSYCHOLOGISTS = frozenset({"bayesian", "black_box"})
+TEACHERS = frozenset({"leitner", "threshold", "sampling"})
 
 NUM_AGENTS = 30
-NUM_TEACHERS = 3
-NUM_LEARNERS = 2
-NUM_PSYCHOLOGISTS = 2
+NUM_TEACHERS = len(TEACHERS)
+NUM_LEARNERS = len(LEARNERS)
+NUM_PSYCHOLOGISTS = len(PSYCHOLOGISTS)
 
 NUM_CONDITIONS = NUM_TEACHERS + NUM_LEARNERS + NUM_PSYCHOLOGISTS
 NUM_OBSERVATIONS = NUM_CONDITIONS * NUM_AGENTS
 
-models = {"learner", "psychologist", "teacher"}
-
-learners = {"exponential", "walsh2018"}
-psychologists = {"bayesian", "black_box"}
-teachers = {"leitner", "threshold", "sampling"}
-
-
-#%% Gen fake data
+# Gen fake data
 def get_trial_conditions():
-    return np.array(tuple(product(learners, psychologists, teachers)))
+    return np.array(tuple(product(LEARNERS, PSYCHOLOGISTS, TEACHERS)))
 
 product_len = len(get_trial_conditions())
 
@@ -55,17 +52,62 @@ t_computation = pd.Series(
     abs(rng.normal(1000, 1000, size=product_len * NUM_AGENTS)),
 ).astype(int)
 
-performance["items_learnt"] = items_learnt
-performance["t_computation"] = t_computation
+performance["Items learnt"] = items_learnt
+performance["Computation time"] = t_computation
 performance["agent"] = np.hstack(np.mgrid[:NUM_AGENTS, :product_len][0])
 
-#%% Chocolate
+
+# Adding more realistic data
+FAKE_SIM_FACTOR = 1.3
+FACTOR_OF_FACTOR = 1.2
+
+teacher_factors = pd.Series(
+    {
+        "leitner": 1,
+        "threshold": FAKE_SIM_FACTOR,
+        "sampling": FAKE_SIM_FACTOR * FACTOR_OF_FACTOR,
+    },
+    name="teacher_factor",
+    dtype=float,
+)
+
+learner_factors = pd.Series(
+    {
+        "exponential": 1,
+        "walsh2018": FAKE_SIM_FACTOR,
+    },
+    name="learner_factor",
+    dtype=float,
+)
+
+psychologist_factors = pd.Series(
+    {
+        "bayesian": 1,
+        "black_box": FAKE_SIM_FACTOR,
+    },
+    name="psychologist_factor",
+    dtype=float,
+)
+
+fake_factors = (teacher_factors, learner_factors, psychologist_factors)
+
+# Add model factors as column and multiply by the values
+for fake_factor in fake_factors:
+    assert "factor" not in performance.columns
+    assert "_" in fake_factor.name
+    model = next(iter(set(performance.columns).intersection(fake_factor.name.split("_"))))
+    performance = pd.merge(performance, fake_factor, how="left", left_on=model, right_index=True)
+    performance["Items learnt"] = performance["Items learnt"] * performance[fake_factor.name]
+    performance["Computation time"] = performance["Computation time"] * performance[fake_factor.name]
+
+
+# Chocolate
 dict_cond_scores = {}
 for index, df in performance.sort_values("agent").groupby(["teacher", "learner", "psychologist"]):
-    dict_cond_scores[frozenset(index)] = df["items_learnt"].values
+    dict_cond_scores[frozenset(index)] = df["Items learnt"].values
 
-teacher_combos = tuple(combinations(teachers, 2))
-learner_psy_combos = tuple(product(learners, psychologists))
+teacher_combos = tuple(combinations(TEACHERS, 2))
+learner_psy_combos = tuple(product(LEARNERS, PSYCHOLOGISTS))
 
 chocolate, axs = plt.subplots(
     len(learner_psy_combos),
@@ -92,18 +134,12 @@ for i, learner_psy_combo in enumerate(learner_psy_combos):
 
 chocolate.savefig(os.path.join(paths.FIG_DIR, "chocolate.pdf"))
 
-
-#%% Efficiency vs. computational cost
-eff_cost_data = performance.groupby("teacher").sum()
-eff_cost = sns.relplot(x="items_learnt", y="t_computation", hue=eff_cost_data.index,  data=eff_cost_data)
-eff_cost.savefig(os.path.join(paths.FIG_DIR, "eff_cost.pdf"))
-
-#%% Other proposals for the same data
-box = sns.catplot(x="teacher", y="items_learnt", kind="box", data=performance)
+# Box
+box = sns.catplot(x="teacher", y="Items learnt", kind="box", data=performance)
 box.savefig(os.path.join(paths.FIG_DIR, "box.pdf"))
 
-swarm = sns.catplot( x="learner", y="items_learnt",hue="teacher", kind="swarm", data=performance)
-swarm.savefig(os.path.join(paths.FIG_DIR, "swarm.pdf"))
+# Efficiency vs. computational cost
+eff_cost_data = performance.groupby("teacher").sum()
+eff_cost = sns.relplot(x="Items learnt", y="Computation time", hue=eff_cost_data.index,  data=eff_cost_data)
+eff_cost.savefig(os.path.join(paths.FIG_DIR, "eff_cost.pdf"))
 
-multibox = sns.catplot( x="learner", y="items_learnt",hue="teacher", kind="box", data=performance)
-multibox.savefig(os.path.join(paths.FIG_DIR, "multibox.pdf"))
