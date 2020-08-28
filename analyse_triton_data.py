5dcece0faa2dbf0fdd4d211da7d75ad41ef6e92d
Aurélien Nioche <nioche.aurelien@gmail.com>
2020-08-28 16:16:28 +0300

    update

--- analyse_triton_data.py
+++ analyse_triton_data.py
@@ -1,51 +1,12 @@
 import os
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
 
 from plot.subplot import chocolate, box, hist
 from settings import paths
+from analysis.preprocess import preprocess_data
 
 FORCE = False
-
-
-def preprocess_data(preprocess_data_file, raw_data_folder):
-
-    assert os.path.exists(raw_data_folder)
-
-    path, dirs, files = next(os.walk(raw_data_folder))
-    file_count = len(files)
-
-    assert file_count > 0
-
-    row_list = []
-
-    for i, p in tqdm(enumerate(os.scandir(raw_data_folder)), total=file_count):
-        df = pd.read_csv(p.path, index_col=[0])
-
-        last_iter = max(df["iter"])
-        is_last_iter = df["iter"] == last_iter
-
-        n_learnt = df[is_last_iter]["n_learnt"].iloc[0]
-
-        is_last_ss = df["ss_idx"] == max(df["ss_idx"]) - 1
-        is_last_iter_ss = df["ss_iter"] == df["ss_n_iter"][0] - 1
-
-        n_learnt_end_ss = df[is_last_ss & is_last_iter_ss]["n_learnt"].iloc[0]
-
-        row = {
-            "Agent ID": i,
-            "Learner": df["md_learner"][0],
-            "Psychologist": df["md_psy"][0],
-            "Teacher": df["md_teacher"][0],
-            "Items learnt one day later": n_learnt,
-            "Items learnt end last session": n_learnt_end_ss,
-        }
-        row_list.append(row)
-
-    df = pd.DataFrame(row_list)
-    df.to_csv(preprocess_data_file)
-    return df
 
 
 def main():
@@ -53,8 +14,6 @@ def main():
     trial_name = input("Trial name: ")
     raw_data_folder = os.path.join(paths.DATA_CLUSTER_DIR, trial_name)
 
-    # RAW_DATA_FOLDER = "/Volumes/niochea1/ActiveTeachingModel/data/triton"
-    # #os.path.join("data", "triton")
     preprocess_data_file = os.path.join(
         "data", "preprocessed", f"chocolate_triton{trial_name}.csv")
 
