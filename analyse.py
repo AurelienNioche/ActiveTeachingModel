b5f4aa1a945fe09ed1c882704aee3229b941df88
Aurélien Nioche <nioche.aurelien@gmail.com>
2020-09-24 18:32:56 +0300

    update

--- analyse.py
+++ analyse.py
@@ -64,7 +64,7 @@ def main(
     force=False,
 ):
 
-    trial_name = "rec-inv-new-post"
+    trial_name = "rec-inv-method3"
 
     root_data_folder = os.path.join("data", "triton", trial_name)
     assert os.path.exists(root_data_folder)
