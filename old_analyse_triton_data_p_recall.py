f886c5847cd562fc1c1976a723423edd2bed02dc
Aurélien Nioche <nioche.aurelien@gmail.com>
2020-09-25 01:59:45 +0300

    update

--- old_analyse_triton_data_p_recall.py
+++ old_analyse_triton_data_p_recall.py
@@ -3,6 +3,8 @@ import os
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from plot.subplot import p_recall_error
 from settings import paths
@@ -23,14 +25,18 @@ def preprocess_data(raw_data_folder, preprocess_file):
         df = pd.read_csv(p.path, index_col=[0])
         df.sort_values("iter", inplace=True)
 
-        for p_err in df["p_err_mean"]:
+        for t, p_err in enumerate(df["p_err_mean"]):
+
+            if np.isnan(p_err):
+                continue
 
             row = {
-                "Agent ID": i,
-                "Learner": df["md_learner"][0],
-                "Psychologist": df["md_psy"][0],
-                "Teacher": df["md_teacher"][0],
-                "p recall error": p_err,
+                "agent": i,
+                # "Learner": df["md_learner"][0],
+                # "Psychologist": df["md_psy"][0],
+                # "Teacher": df["md_teacher"][0],
+                "p_err_mean": p_err,
+                "time": t,
             }
             row_list.append(row)
 
@@ -43,35 +49,70 @@ def main():
 
     force = True
 
-    trial_name = "lin_Nomni_spec"  # input("Trial name: ")
-    raw_data_folder = os.path.join(paths.DATA_CLUSTER_DIR, trial_name)
+    trial_name = "kiwi"  # input("Trial name: ")
+    for subfolder in "Nspec-Nomni", "spec-Nomni":
+        for teacher in "recursive_inverse", "threshold":
 
-    preprocess_file = os.path.join("data", "preprocessed", f"{trial_name}_p_recall.csv")
+            teacher_folder = f"exp_decay-psy_grid-{teacher}"
+            raw_data_folder = os.path.join("data", "triton", trial_name, subfolder, teacher_folder)
+            assert os.path.exists(raw_data_folder)
 
-    fig_folder = os.path.join("fig", trial_name)
-    fig_path = os.path.join(fig_folder, f"{trial_name}_p_recall.pdf")
+            preprocessed_folder = os.path.join("data", "preprocessed", "analyse_p_recall")
 
-    os.makedirs(os.path.join("data", "preprocessed"), exist_ok=True)
-    os.makedirs(fig_folder, exist_ok=True)
+            preprocess_file = os.path.join(preprocessed_folder, f"{trial_name}_{subfolder}_{teacher}_p_recall.csv")
 
-    if not os.path.exists(preprocess_file) or force:
-        df = preprocess_data(
-            raw_data_folder=raw_data_folder, preprocess_file=preprocess_file
-        )
-    else:
-        df = pd.read_csv(preprocess_file, index_col=[0])
+            fig_folder = os.path.join("fig", "analyse_p_recall")
+            fig_path = os.path.join(fig_folder, f"{trial_name}_{subfolder}_{teacher}_p_recall.pdf")
 
-    teachers = sorted(np.unique(df["Teacher"]))
-    learners = sorted(np.unique(df["Learner"]))
-    psy = sorted(np.unique(df["Psychologist"]))
+            for f in preprocessed_folder, fig_folder:
+                os.makedirs(f, exist_ok=True)
 
-    p_recall_error.plot(
-        teachers=teachers,
-        learners=learners,
-        psychologists=psy,
-        df=df,
-        fig_path=fig_path,
-    )
+            if not os.path.exists(preprocess_file) or force:
+                df = preprocess_data(
+                    raw_data_folder=raw_data_folder,
+                    preprocess_file=preprocess_file)
+            else:
+                df = pd.read_csv(preprocess_file, index_col=[0])
+
+            # teachers = sorted(np.unique(df["Teacher"]))
+            # learners = sorted(np.unique(df["Learner"]))
+            # psy = sorted(np.unique(df["Psychologist"]))
+
+            # p_recall_error.plot(
+            #     teachers=teachers,
+            #     learners=learners,
+            #     psychologists=psy,
+            #     df=df,
+            #     fig_path=fig_path)
+
+                # Text psychologist name to column
+                # Text learner and teacher combo to row
+            fig, ax = plt.subplots()
+
+            sns.lineplot(data=df, x="time", y="p_err_mean", ci="sd")
+
+            # y = [_, df["p_mean_err"].group_by("iter")
+            #
+            # mean_y = np.mean(y, axis=0)
+            # std_y = np.std(y, axis=0)
+            #
+            # color = "C0"
+            #
+            # ax.plot(mean_y, color=color)
+            #
+            # ax.fill_between(
+            #     np.arange(len(mean_y)),
+            #     mean_y + std_y,
+            #     mean_y - std_y,
+            #     alpha=0.5,
+            #     color=color,
+            # )
+            #
+            # ax.set_ylim((0, 1))
+            # ax.set_xlabel("Time")
+
+            ax.set_ylabel(f"p recall error")
+            plt.savefig(fig_path)
 
 
 if __name__ == "__main__":
