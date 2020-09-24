893bbe01898dc3f95348e5cc4033cf2aea60ed6e
Aurélien Nioche <nioche.aurelien@gmail.com>
2020-09-25 02:34:00 +0300

    update

--- analyse.py
+++ analyse.py
@@ -64,7 +64,7 @@ def main(
     force=False,
 ):
 
-    trial_name = "kiwi"
+    trial_name = "noprior"
 
     root_data_folder = os.path.join("data", "triton", trial_name)
     assert os.path.exists(root_data_folder)
--- analyse_p_recall.py
+++ analyse_p_recall.py
@@ -3,6 +3,8 @@ import os
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from plot.subplot import p_recall_error
 from settings import paths
@@ -10,16 +12,9 @@ from settings import paths
 
 def preprocess_data(raw_data_folder, preprocess_file):
 
-    cond_data_folder = raw_data_folder
-
-    teach_f = [
-        p for p in os.scandir(cond_data_folder.path) if not p.name.startswith(".")
-    ]
-
-    # assert os.path.exists(raw_data_folder)
+    assert os.path.exists(raw_data_folder)
 
     path, dirs, files = next(os.walk(raw_data_folder))
-    print(raw_data_folder)
     file_count = len(files)
 
     assert file_count > 0
@@ -30,14 +25,18 @@ def preprocess_data(raw_data_folder, preprocess_file):
         df = pd.read_csv(p.path, index_col=[0])
         df.sort_values("iter", inplace=True)
 
-        for p_err in df["p_err_mean"]:
+        for t, p_err in enumerate(df["p_err_mean"]):
+
+            if np.isnan(p_err):
+                continue
 
             row = {
-                "Agent ID": i,
-                "Learner": df["md_learner"][0],
-                "Psychologist": df["md_psy"][0],
-                "Teacher": df["md_teacher"][0],
-                "p recall error": p_err,
+                "agent": i,
+                # "Learner": df["md_learner"][0],
+                # "Psychologist": df["md_psy"][0],
+                # "Teacher": df["md_teacher"][0],
+                "p_err_mean": p_err,
+                "time": t,
             }
             row_list.append(row)
 
@@ -46,51 +45,40 @@ def preprocess_data(raw_data_folder, preprocess_file):
     return df
 
 
-def main():
+def main(force=False):
 
-    force = True
+    trial_name = "noprior"  # input("Trial name: ")
+    for subfolder in "Nspec-Nomni", "spec-Nomni":
+        for teacher in "recursive_inverse", "threshold":
 
-    trial_name = "kiwi"  # input("Trial name: ")
-    raw_data_folder = os.path.join(paths.DATA_CLUSTER_DIR, trial_name)
+            teacher_folder = f"exp_decay-psy_grid-{teacher}"
+            raw_data_folder = os.path.join("data", "triton", trial_name, subfolder, teacher_folder)
+            assert os.path.exists(raw_data_folder)
 
-    preprocess_file = os.path.join("data", "preprocessed", f"{trial_name}_p_recall.csv")
+            preprocessed_folder = os.path.join("data", "preprocessed", "analyse_p_recall")
 
-    fig_folder = os.path.join("fig", trial_name)
-    fig_path = os.path.join(fig_folder, f"{trial_name}_p_recall.pdf")
+            preprocess_file = os.path.join(preprocessed_folder, f"{trial_name}_{subfolder}_{teacher}_p_recall.csv")
 
-    os.makedirs(os.path.join("data", "preprocessed"), exist_ok=True)
-    os.makedirs(fig_folder, exist_ok=True)
+            fig_folder = os.path.join("fig", "analyse_p_recall")
+            fig_path = os.path.join(fig_folder, f"{trial_name}_{subfolder}_{teacher}_p_recall.pdf")
 
-    preprocess_folder = os.path.join("data", "preprocessed", trial_name)
-    os.makedirs(preprocess_folder, exist_ok=True)
+            for f in preprocessed_folder, fig_folder:
+                os.makedirs(f, exist_ok=True)
 
-    root_data_folder = raw_data_folder
-    cond_f = [p for p in os.scandir(root_data_folder) if not p.name.startswith(".")]
+            if not os.path.exists(preprocess_file) or force:
+                df = preprocess_data(
+                    raw_data_folder=raw_data_folder,
+                    preprocess_file=preprocess_file)
+            else:
+                df = pd.read_csv(preprocess_file, index_col=[0])
 
-    for _, cp in enumerate(cond_f):
+            fig, ax = plt.subplots()
 
-        # cp: cond_path
-        print("cond data folder:", cp.name)
+            sns.lineplot(data=df, x="time", y="p_err_mean", ci="sd", ax=ax)
 
-        pp_data_file = os.path.join(preprocess_folder, f"{cp.name}.csv")
-        # pp: preproc_path
-
-        if not os.path.exists(preprocess_file) or force:
-            df = preprocess_data(raw_data_folder=cp, preprocess_file=pp_data_file)
-        else:
-            df = pd.read_csv(pp_data_file, index_col=[0])
-
-    teachers = sorted(np.unique(df["Teacher"]))
-    learners = sorted(np.unique(df["Learner"]))
-    psy = sorted(np.unique(df["Psychologist"]))
-
-    p_recall_error.plot(
-        teachers=teachers,
-        learners=learners,
-        psychologists=psy,
-        df=df,
-        fig_path=fig_path,
-    )
+            ax.set_ylabel(f"p recall error")
+            ax.set_ylim(0, 1)
+            plt.savefig(fig_path)
 
 
 if __name__ == "__main__":
